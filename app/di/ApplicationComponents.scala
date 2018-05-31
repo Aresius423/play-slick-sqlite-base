4e7fd2583f8464ba4d4b22ccd4ca6f66987e4b49
Markus Janos Daniel <aresius423@gmail.com>
2018-05-31 15:30:31 +0200

    Migrated to play 2.6

--- app/di/ApplicationComponents.scala
+++ app/di/ApplicationComponents.scala
@@ -2,26 +2,25 @@ package di
 
 import controllers.HomeController
 import play.api.ApplicationLoader.Context
+import play.api.db.DBApi
 import play.api.db.evolutions.EvolutionsComponents
-import play.api.db.slick.SlickComponents
-import play.api.db.slick.evolutions.SlickEvolutionsComponents
+import play.api.db.slick.{SlickComponents, SlickApi}
+import play.api.db.slick.evolutions.{SlickEvolutionsComponents, SlickDBApi}
 import play.api.mvc.EssentialFilter
 import play.api.routing.Router
 import play.api.{BuiltInComponentsFromContext, Configuration}
+import play.controllers.AssetsComponents
 
 class ApplicationComponents(context: Context)
   extends BuiltInComponentsFromContext(context)
-//    with AssetsComponents
+    with controllers.AssetsComponents
     with EvolutionsComponents
     with SlickComponents
-    with SlickEvolutionsComponents
     with ControllerComponents
     with RulesComponents {
 
-  private lazy val assets = new controllers.Assets(httpErrorHandler)
-
   override def router: Router = new _root_.router.Routes(httpErrorHandler, homeController, assets)
-
+  override lazy val dbApi: DBApi = SlickDBApi(slickApi)
   override lazy val httpFilters: Seq[EssentialFilter] = Seq.empty
 
   applicationEvolutions // force evolutions
